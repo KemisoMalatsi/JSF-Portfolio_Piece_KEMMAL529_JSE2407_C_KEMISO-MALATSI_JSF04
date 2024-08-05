13046210ee0c605010df845bb8189b2af68fbd48
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-08-05 18:57:01 +0200

    Refactor Header.vue component: simplify navigation, remove unnecessary classes and styles, and update SVG icons and router links.

--- my-vue-challenge/src/components/Header.vue
+++ my-vue-challenge/src/components/Header.vue
@@ -1,103 +1,43 @@
 <template>
-  <header class="sticky z-50 top-0">
-    <nav class="bg-gray-500 border-gray-200 p-0">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <router-link to="/">
-          <button class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="/online-shop.png" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
-              SwiftCart
-            </span>
-          </button>
-        </router-link>
-        <button
-          @click="toggleNavbar"
-          data-collapse-toggle="navbar-default"
-          type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-
-        <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-          <ul class="flex flex-col top-10 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-500 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>
-              <router-link
-                to=""
-                class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >
-                Wishlist
-              </router-link>
-            </li>
-            <router-link to="">
-              <li class="hidden lg:block md:block relative">
-                <div class="t-0 absolute left-3 -top-4">
-                  <p class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">
-                    2
-                  </p>
-                </div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="file: h-6 w-6 stroke-white cursor-pointer"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                  />
-                </svg>
-              </li>
+  <header class="sticky top-0 bg-gray-500 z-50">
+    <nav class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="../assets/online-shop.png" class="h-8" alt="SwiftCart Logo" />
+        <span class="text-2xl font-semibold text-white">SwiftCart</span>
+      </router-link>
+      <button @click="isOpen = !isOpen" type="button" class="text-white md:hidden">
+        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
+      <div :class="{ 'block': isOpen, 'hidden': !isOpen }" class="w-full md:block md:w-auto">
+        <ul class="flex flex-col md:flex-row md:space-x-8">
+          <li><router-link to="/wishlist" class="text-white md:hover:text-blue-700">Wishlist</router-link></li>
+          <li class="relative hidden md:block">
+            <router-link to="/cart" class="text-white md:hover:text-blue-700">
+              <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+              </svg>
             </router-link>
-            <li>
-              <router-link
-                to=""
-                class="lg:hidden md:hidden py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >
-                Cart
-              </router-link>
-            </li>
-
-            <li>
-              <router-link
-                to="login"
-                class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >
-                Login
-              </router-link>
-            </li>
-          </ul>
-        </div>
+          </li>
+          <li><router-link to="/cart" class="text-white md:hidden">Cart</router-link></li>
+          <li><button @click="$emit('toggle-login')" class="text-white">Login</button></li>
+        </ul>
       </div>
     </nav>
   </header>
 </template>
 
-<script setup>
-const toggleNavbar = () => {
-  const dropDown = document.getElementById('navbar-dropdown');
-  dropDown.classList.contains('hidden')
-    ? dropDown.classList.remove('hidden')
-    : dropDown.classList.add('hidden');
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  }
 };
 </script>
+
+<style scoped>
+/* Add any additional styles you need here */
+</style>
