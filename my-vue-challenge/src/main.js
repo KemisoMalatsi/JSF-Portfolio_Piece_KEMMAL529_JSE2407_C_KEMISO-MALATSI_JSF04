5b99bab112dc9f47f24a9ed06b16b4b89dcd3cbf
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-08-05 15:11:48 +0200

    Added ProductCard.vue component and modified main.js to comment out Pinia import

--- my-vue-challenge/src/main.js
+++ my-vue-challenge/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 // import router from './router';
 import './tailwind.css';
-import { createPinia } from 'pinia';
+// import { createPinia } from 'pinia';
 
 const app = createApp(App);
 
